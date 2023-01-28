6e930cc50fcb63189368bca8ef933947e7837f02
Veysel Aydin <weycell.aydin09@gmail.com>
2023-01-28 14:47:30 +0300

    app2

--- app.js
+++ app.js
@@ -12,3 +12,25 @@ document.getElementById("table").onclick = (e) => {
     result();
   }
 };
+const writingCurrentArea = (e) => {
+  if (e === "." && currentArea.innerText.includes(".")) return;
+  if (e === "-" && currentArea.innerText.includes("-")) return;
+  currentArea.innerText += e;
+};
+
+const deleteCurrentArea = (e) => {
+  if (e === "AC") {
+    currentArea.innerText = "";
+    previousArea.innerText = "";
+  } else if (e === "DEL") {
+    currentArea.innerText = currentArea.innerText.slice(0, -1);
+  }
+};
+const selectOperation = (e) => {
+  if (currentArea.innerText === "") return;
+  if (previousArea.innerText !== "") {
+    calculate();
+  }
+  previousArea.innerText = currentArea.innerText + e;
+  currentArea.innerText = "";
+};
